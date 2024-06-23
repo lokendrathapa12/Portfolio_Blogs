2c58bcea7e4eb065f1651687f4e4198749f46319
lokendrathapa12 <lokendrathapa056@gmail.com>
2024-06-23 22:40:19 +0545

    last commit

--- project2/settings.py
+++ project2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+&zo^)0h!@pmkj^u1cd7=6nvfsalf5n$27_$b^ick=n=&z8lad
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
